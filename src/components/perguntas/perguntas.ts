2c90957744066f1592162e8cd8bfbc83728a8bf3
Cayo Hollanda <cayohollanda@hotmail.com>
2018-09-07 11:42:57 -0300

    Alterando texto de dica em perguntas.html e revendo imports desnecessários

--- src/components/perguntas/perguntas.ts
+++ src/components/perguntas/perguntas.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlertController, ToastController, ModalController, NavController, Platform } from 'ionic-angular';
-import { HomePage } from '../../pages/home/home';
 
 /**
  * Generated class for the PerguntasComponent component.
